d6e6c4c386dfda400ae51dcc3fbcb15b084ae369
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-12-05 11:50:34 +0530

    Update config.py

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -19,7 +19,7 @@ import os
 from yarl import URL
 
 try:
-    port = int("8080")
+    port = int(os.environ.get("PORT", "8080"))
 except ValueError:
     port = -1
 if not 1 <= port <= 65535:
@@ -27,32 +27,32 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["2803227"])
-    api_hash = os.environ["2a7518366d9f395750232e8f21ad85dc"]
+    api_id = int(os.environ["TG_API_ID"])
+    api_hash = os.environ["TG_API_HASH"]
 except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
     sys.exit(1)
 
-trust_headers = "False"
-host = os.environ["localhost"]
+trust_headers = bool(os.environ.get("TRUST_FORWARD_HEADERS"))
+host = os.environ.get("HOST", "localhost")
 public_url = URL(os.environ.get("PUBLIC_URL", f"http://{host}:{port}"))
 
-session_name = os.environ["tgfilestream"]
+session_name = os.environ.get("TG_SESSION_NAME", "tgfilestream")
 
 log_config = os.environ.get("LOG_CONFIG")
-debug = "True"
+debug = bool(os.environ.get("DEBUG"))
 
 try:
     # The per-user ongoing request limit
-    request_limit = int("5")
+    request_limit = int(os.environ.get("REQUEST_LIMIT", "5"))
 except ValueError:
     print("Please make sure the REQUEST_LIMIT environment variable is an integer")
     sys.exit(1)
 
 try:
     # The per-DC connection limit
-    connection_limit = int(os.environ("20"))
+    connection_limit = int(os.environ.get("CONNECTION_LIMIT", "20"))
 except ValueError:
     print("Please make sure the CONNECTION_LIMIT environment variable is an integer")
     sys.exit(1)
